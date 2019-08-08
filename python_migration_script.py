acb7a6c1e2b5107e23d39d03a0ba85729114d686
jeor0980 <jesusotovar@hotmail.com>
2019-08-08 12:40:41 -0600

    Added steps to deploy to migrated site to live environment

--- python_migration_script.py
+++ python_migration_script.py
@@ -3,6 +3,18 @@ from local_vars import *
 import subprocess
 import requests
 import time
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option('-f', '--nofiles',
+                  action="store_false", dest="import_site_files",
+                  help="Boolean to switch, use this flag to exclude files on site import")
+
+options, args = parser.parse_args()
+
+# Bool, False will skip files from being imported
+import_file_bool = options.import_site_files
 
 # Open file where we log sites
 f = open("imported_sites.txt", "a+")
@@ -10,17 +22,16 @@ f = open("imported_sites.txt", "a+")
 # Atlas target Environment
 env = ""
 # TODO: Get rid of the backups_env var, only used when scp
-backups_env = ""
-# Pantheon Target Environment
-pantheon_env = ""
-# Pantheon Test Environment
-pantheon_deploy_env = ""
+backups_env = "prod"
+
+# TODO: Agree on prefix
+site_prefix = "ucb"
+
+# Number of seconds to wait before checking if site backup is ready
+backup_wait = 5
 
 # Instance to export
 instance_list = [""]
-
-# Backup Timer
-backup_wait = 5
 
 for instance in instance_list:
     
@@ -41,7 +52,7 @@ for instance in instance_list:
 
     # Parse url, replace forward slashes with dashes
     # The site name can only contain a-z, A-Z, 0-9, and dashes ('-'), cannot begin or end with a dash, and must be fewer than 52 characters
-    pantheon_site_name = site_name.replace("/", "-")
+    pantheon_site_name = site_prefix + '-' + site_name.replace("/", "-")
     print(f"Pantheon site name: {pantheon_site_name}")
 
     # Label will be the site name for now
@@ -69,13 +80,17 @@ for instance in instance_list:
     site_database = site_backup_json["_items"][0]["database"]
 
     # User needs to have SSH key added to server in order for scp to work
-    print(f"Exporting files from {env}...")
+    if (import_file_bool == False): 
+        print('Skipping file imports...')
+    else:
+        print(f"Exporting files from {env}...")
+        print(f"Files Backup: {site_files}")
+        subprocess.call(["scp", "-i" "~/.ssh/id_rsa", f"{env}:/nfs/{backups_env}_backups/backups/{site_files}", "./files"])
 
-    subprocess.call(["scp", "-i", "~/.ssh/id_rsa", f"{env}:/nfs/{backups_env}_backups/backups/{site_database}", "./database"])
     print(f"Database Backup: {site_database}")
+    subprocess.call(["scp", "-i", "~/.ssh/id_rsa", f"{env}:/nfs/{backups_env}_backups/backups/{site_database}", "./database"])
 
-    subprocess.call(["scp", "-i" "~/.ssh/id_rsa", f"{env}:/nfs/{backups_env}_backups/backups/{site_files}", "./files"])
-    print(f"Files Backup: {site_files}")
+    
 
     # Pantheon create new site
     # site:create [--org [ORG]] [--region [REGION]] [--] <site> <label> <upstream_id>
@@ -97,22 +112,45 @@ for instance in instance_list:
     database_sync.wait()
     print("Database upload complete")
 
-    print("Rsync'ing files to pantheon")
-    # # Unzip files backup on local machine
-    unpack_files = subprocess.call(["tar", "-xzf", f"./files/{site_files}", "-C", "./files/"])
+    if (import_file_bool == False):
+        print('Skipping file rsync...')
+    else:
+
+        print("Rsync'ing files to pantheon")
+        # # Unzip files backup on local machine
+        unpack_files = subprocess.call(["tar", "-xzf", f"./files/{site_files}", "-C", "./files/"])
     
-    # Remove tar file
-    remove_tar = subprocess.call(["rm", f"./files/{site_files}"])
+        # Remove tar file
+        remove_tar = subprocess.call(["rm", f"./files/{site_files}"])
 
-    # TODO: Verify $env substitution, handle errors or write script to restart rync
-    # Rsync files to pantheon
-    file_rsync = subprocess.Popen([f'rsync -rlIpz -e "ssh -p 2222 -o StrictHostKeyChecking=no" --temp-dir=~/tmp --delay-updates ./files/ {pantheon_env}.{site_id}@appserver.{pantheon_env}.{site_id}.drush.in:files'], shell=True)
-    file_rsync.wait()
-    print("File rsync complete")
+        # TODO: Verify $env substitution, handle errors or write script to restart rync
+        # Rsync files to pantheon
+        file_rsync = subprocess.Popen([f'rsync -rlIpz -e "ssh -p 2222 -o StrictHostKeyChecking=no" --temp-dir=~/tmp --delay-updates ./files/ dev.{site_id}@appserver.dev.{site_id}.drush.in:files'], shell=True)
+        file_rsync.wait()
+        print("File rsync complete")
 
-    print(f"Deploying {pantheon_site_name} to {pantheon_deploy_env} environment")
-    deploy_test_env = subprocess.Popen([f"terminus env:deploy --updatedb {pantheon_site_name}.{pantheon_deploy_env}"], shell=True)
+    # TODO:
+    # settings.php
+
+    # Disable ucb_on_prem_hosting module
+    enable_ucb_on_prem = subprocess.Popen([f'terminus drush {pantheon_site_name}.dev pm-disable ucb_on_prem_hosting'], shell=True)
+    enable_ucb_on_prem.wait()
+
+    # Enable pantheon_hosting_module
+    enable_ucb_on_prem = subprocess.Popen([f'terminus drush {pantheon_site_name}.dev pm-enable pantheon_hosting'], shell=True)
+    enable_ucb_on_prem.wait()
+
+    print(f"Deploying {pantheon_site_name} to test environment")
+    deploy_test_env = subprocess.Popen([f"terminus env:deploy --updatedb {pantheon_site_name}.test"], shell=True)
     deploy_test_env.wait()
+
+    print(f"Deploying {pantheon_site_name} to prod environment")
+    deploy_prod_env = subprocess.Popen([f"terminus env:deploy --updatedb {pantheon_site_name}.prod"], shell=True)
+    deploy_prod_env.wait()
+
+    # Enable redis server for site
+    enable_redis = subprocess.Popen([f"terminus redis:enable {site_id}"], shell=True)
+    enable_redis.wait()
 
     # Clean up 
     print("Cleaning up for next run...")
