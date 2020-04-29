5a0e8f4d525d58c8e91a4ae5f886954e0e9594eb
jeor0980 <jesusotovar@hotmail.com>
2020-04-29 16:58:55 -0600

    Initial code to teplate simplesamlphp config.php file

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 """Migration Helper Functions."""
+import os
 import requests
+from jinja2 import Environment, PackageLoader
 
 
 def create_pantheon_site(auth_token, sid, path, instance_type, pantheon_size, user):
@@ -15,5 +17,20 @@ def create_pantheon_site(auth_token, sid, path, instance_type, pantheon_size, us
     request_headers = {'Authorization': auth_token}
 
     walnut_request = requests.post(
-        'http://127.0.0.1:5000/instance', headers=request_headers, json=site_payload)
+        'http://34.82.22.38/instance', headers=request_headers, json=site_payload)
     return walnut_request.status_code
+
+
+def generate_simplesaml_config(base_url_path, session_cookie_path, session_cookie_domain):
+    """Generate a the config file for simplesaml with hardcoded host values"""
+    settings_variables = {"BASE_URL_PATH": base_url_path, "SESSION_COOKIE_PATH": session_cookie_path, "SESSION_COOKIE_DOMAIN": session_cookie_domain}
+    jinja_env = Environment(loader=PackageLoader('migration_utils', 'templates'))
+    template = jinja_env.get_template('config.php')
+    render = template.render(settings_variables)
+
+    # Remove the existing file.
+    if os.access("/config.php", os.F_OK):
+        os.remove("/config.php")
+    # Write the render to a file.
+    with open("/config.php", "wb") as open_file:
+        open_file.write(render)
